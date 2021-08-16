03cbf28e8111121a46676d17426821475f13f92f
PoojaGupta4428 <88989526+PoojaGupta4428@users.noreply.github.com>
2021-08-16 13:29:59 +0530

    Add files via upload

---
+++ parkingmanagmentdb (1).sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 16, 2021 at 09:59 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `parkingmanagmentdb`
+--
+CREATE DATABASE IF NOT EXISTS `parkingmanagmentdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `parkingmanagmentdb`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parkinglot`
+--
+
+DROP TABLE IF EXISTS `parkinglot`;
+CREATE TABLE `parkinglot` (
+  `pid` int(11) NOT NULL,
+  `Is_reserved` enum('0','1') NOT NULL,
+  `Is_occupied` enum('0','1') NOT NULL DEFAULT '0',
+  `last_user_id` int(11) DEFAULT NULL,
+  `last_bookinng_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `category` enum('gen','ph','pw') NOT NULL,
+  `is_active` enum('1','0') NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userbookings`
+--
+
+DROP TABLE IF EXISTS `userbookings`;
+CREATE TABLE `userbookings` (
+  `bookinng_id` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `Booking_start_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `booking_valid_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL,
+  `vehical_number` varchar(36) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `parkinglot`
+--
+ALTER TABLE `parkinglot`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userbookings`
+--
+ALTER TABLE `userbookings`
+  ADD PRIMARY KEY (`bookinng_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `pid` (`pid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `parkinglot`
+--
+ALTER TABLE `parkinglot`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+--
+-- AUTO_INCREMENT for table `userbookings`
+--
+ALTER TABLE `userbookings`
+  MODIFY `bookinng_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `userbookings`
+--
+ALTER TABLE `userbookings`
+  ADD CONSTRAINT `userbookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `userbookings_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `parkinglot` (`pid`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
